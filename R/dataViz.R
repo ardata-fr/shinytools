c3d53f25433e1aa0bca52022011ca4881eee5fde
David Gohel <david.gohel@ardata.fr>
2019-05-01 15:32:57 +0200

    clean rename and doc

--- R/dataViz.R
+++ R/dataViz.R
@@ -1,30 +1,51 @@
 #' @export
 #' @title Module dataViz UI part
 #' @description UI part of module import
-#' @param id Module call id
-#' @import shiny
-dataVizUI <- function(id) {
+#' @param id namespace identifier for the module
+#' @importFrom shiny NS uiOutput req
+#' @rdname dataViewer
+dataViewerUI <- function(id) {
   ns <- NS(id)
-
   uiOutput(ns("ui_DT_viz"))
 }
 
 #' @export
-#' @title Module dataViz Server part
-#' @description Server part of module dataViz
-#' @param input Not a real parameter, not to be set manually.
-#' @param output Not a real parameter, not to be set manually.
-#' @param session Not a real parameter, not to be set manually.
-#' @param data Reactive data.frame
-#' @param part "All" to show all table, "header" & "footer" to show head and/or tail
-#' @param style Style that defines how to display DT table (only minimal & default available for the moment)
-#' @import shiny
+#' @title shiny UI to view data
+#' @description A module to view data in a DataTable output. An option is available
+#' to display only first or last rows of the dataset.
+#' @param input,output,session mandatory arguments for modules to be valid. These
+#' should not to be defined as they will be handled by shiny.
+#' @param data the input data.frame to be displayed. It must be a reactive
+#' value.
+#' @param part one of "all" to show all table, c("header", "footer") to show first and last rows, "header"
+#' to show only first rows, "footer" to show only last rows.
+#' @param style define how to display the DataTable ("minimal" or "default")
 #' @importFrom utils head tail
-dataVizServer <- function(input, output, session, data = reactive(NULL), part = "all", style = "minimal") {
+#' @importFrom DT renderDataTable datatable dataTableOutput
+#' @importFrom shiny reactive
+#' @rdname dataViewer
+#' @examples
+#' library(shinytools)
+#' library(shiny)
+#'
+#' # example with dataViewerUI and dataViewerServer ----
+#' if (interactive()) {
+#'   options(device.ask.default = FALSE)
+#'
+#'   ui <- fluidPage(
+#'     fluidRow(column(width = 12, dataViewerUI(id = "demo") ) )
+#'   )
+#'
+#'   server <- function(input, output, session) {
+#'     callModule(module = dataViewerServer, id = "demo",
+#'                data = reactive(iris), part = c("header", "footer"))
+#'   }
+#'   print(shinyApp(ui, server))
+#' }
+#'
+dataViewerServer <- function(input, output, session, data = reactive(NULL), part = "all", style = "minimal") {
   ns <- session$ns
-  
-  requireNamespace(package = "DT", quietly = TRUE)
-  
+
   getViz <- function(data, part) {
     if (all(c("header", "footer") %in% part)) {
       cast_to_short_table(x = data, symbol = "...")
@@ -39,11 +60,11 @@ dataVizServer <- function(input, output, session, data = reactive(NULL), part = 
     }
   }
 
-  output$DT_viz <- DT::renderDataTable({
+  output$DT_viz <- renderDataTable({
     req(data())
     dat <- getViz(data = data(), part = part)
     if (style == "minimal") {
-      DT::datatable(dat,
+      datatable(dat,
         rownames = FALSE,
         options =   list(dom = 'tipr',
                 ordering = FALSE,
@@ -54,8 +75,8 @@ dataVizServer <- function(input, output, session, data = reactive(NULL), part = 
               )
       )
     } else {
-      DT::datatable(dat, 
-        filter = 'top', 
+      datatable(dat,
+        filter = 'top',
         options = list(
           pageLength = 10,
           scrollX = TRUE,
@@ -68,16 +89,16 @@ dataVizServer <- function(input, output, session, data = reactive(NULL), part = 
 
   output$ui_DT_viz <- renderUI({
     if (is.null(data())) {
-      tags$div(class = "alert alert-warning", role = "alert",
+      div(class = "alert alert-warning", role = "alert",
         "No dataset loaded"
       )
     } else if (isFALSE(data())) {
-      tags$div(class = "alert alert-danger", role = "alert",
+      div(class = "alert alert-danger", role = "alert",
         "Error when loading dataset, check your import options"
       )
     } else {
-      tags$div(
-        DT::dataTableOutput(ns("DT_viz"))
+      div(
+        dataTableOutput(ns("DT_viz"))
       )
     }
   })
